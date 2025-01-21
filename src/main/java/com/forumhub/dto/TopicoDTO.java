24d3737049d0c09dd10817cdb49a95049626919e
KgbArmageddon <karimydesouzaborda@gmail.com>
2025-01-21 16:10:33 -0300

    Adicionado TopicoDTO.java

---
+++ src/main/java/com/forumhub/dto/TopicoDTO.java
@@ -0,0 +1,10 @@
+package com.forumhub.dto;
+
+public class TopicoDTO {
+    private String titulo;
+    private String mensagem;
+    private String autor;
+    private String curso;
+
+    // Getters e Setters
+}
